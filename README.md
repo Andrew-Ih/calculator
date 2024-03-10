9d90812f8a51b38561177a2d2ffa21d5f09491d6
Andrew.I <andrewiheanacho811@gmail.com>
2024-03-09 18:37:24 -0600

    implement calculator operations

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # calculator
+
+Project: Calculator
+
+Description: This project is a simple calculator that performs the basic
+             arithmetic operations.
+
+
+Languages: 
+1. HTML 
+2. CSS 
+3. JavaScript
+
+Used CSS and HTML to create and style the calculator objects.
+Then followed up with JavaScript to implement the functioning 
+of the calculator.
---
+++ index.js
@@ -0,0 +1,87 @@
+
+let firstNum = '';
+let operator = '';
+let secondNum = '';
+let result = 0;
+
+const numbers = document.querySelectorAll('.buttons #num');
+
+const display = document.querySelector('.display');
+
+const operators = document.querySelectorAll('.buttons .operator');
+
+const clear = document.querySelector('.clear');
+
+
+function add(num1, num2) {
+    return (num1 + num2);
+}
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+}
+
+function multiply(num1, num2) {
+    return (num1 * num2);
+}
+
+function divide (num1, num2) {
+    return (num1/num2).toFixed(3);
+}
+
+function operate (num1, num2, operator) {
+    let final = 0;
+    if (operator === '+'){
+        final = add(num1, num2);
+    } else if (operator === '-') {
+        final = subtract(num1, num2);
+    } else if (operator === 'x') {
+        final = multiply(num1, num2);
+    } else if (operator === '÷') {
+        final = divide (num1, num2);
+    } 
+
+    return final;
+}
+
+function updateDisplay (symbol) {
+    display.textContent = symbol;
+}
+
+numbers.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        if (operator === '') {
+            firstNum += event.target.textContent;
+            updateDisplay(firstNum);
+        } else {
+            secondNum += event.target.textContent;
+            updateDisplay(secondNum);
+            result = operate(parseInt(firstNum), parseInt(secondNum), operator);
+        }
+    });
+});
+
+
+operators.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const symbol = (event.target.textContent);
+        operator = symbol;
+        if (symbol === '=') {
+            updateDisplay(result);
+            firstNum = "";
+            secondNum = "";
+            operator = "";
+        } 
+    });
+});
+
+function clearDisplay () {
+    clear.addEventListener('click', () => {
+        updateDisplay(" ");
+        firstNum = "";
+        secondNum = "";
+        operator = "";
+    });
+}
+
+clearDisplay();
